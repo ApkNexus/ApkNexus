1fbdcf174c99710019417af4a8955566d814df40
ApkNexus <apknexus4@gmail.com>
2025-11-27 00:10:16 -0500

    Update appPage.js

--- scripts/appPage.js
+++ scripts/appPage.js
@@ -32,8 +32,8 @@ fetch('data/apps.json')
         </div>
 
         <div class="download-section" data-aos="zoom-in">
-          <a href="download.html?url=${encodeURIComponent(app.download)}" 
-             class="btn-download" target="_blank">⬇ Descargar APK</a>
+          <a href="download.html?id=${encodeURIComponent(app.id)}&url=${encodeURIComponent(app.download)}"
+             class="btn-download">⬇ Descargar APK</a>
         </div>
       </div>
     `;
